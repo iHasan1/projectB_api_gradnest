e1215cfd2c969c429670d63b9bf22b6c89d8c613
buttuxama <93969159+buttuxama@users.noreply.github.com>
2021-11-12 16:41:19 +0500

    Add files via upload
    The 'Deactivate Functionality" added.

---
+++ users.routes.js
@@ -0,0 +1,12 @@
+const usersController = require("../controllers/user.controller");
+
+const express = require("express");
+const router = express.Router();
+
+router.post("/register", usersController.register);
+router.post("/login", usersController.login);
+router.get("/user-Profile/:email", usersController.userProfile);
+router.get("/update-user", usersController.updateUser);
+router.post("/deactivate-user", usersController.deactivateUser);
+
+module.exports = router;
